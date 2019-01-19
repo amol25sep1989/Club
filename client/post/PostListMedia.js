1427cb3664eac60697fffbc771bf25940dc8bba5
amol25sep1989 <46740435+amol25sep1989@users.noreply.github.com>
2019-01-19 15:22:07 +0530

    Update and rename Postlist.1.js to PostListMedia
    Update and rename Postlist.1.js to PostListMedia

--- client/post/PostListMedia.js
+++ client/post/PostListMedia.js
@@ -1,22 +1,22 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {withStyles} from 'material-ui/styles'
-import Post1 from './Post.1'
+import PostMedia from './PostMedia'
 
-class PostList1 extends Component {
+class PostListMedia extends Component {
   render() {
     return (
       <div style={{marginTop: '24px'}}>
         {this.props.posts.map((item, i) => {
-            return <Post1 post={item} key={i} onRemove={this.props.removeMediaUpdate}/>
+            return <PostMedia post={item} key={i} onRemove={this.props.removeMediaUpdate}/>
           })
         }
       </div>
     )
   }
 }
-PostList1.propTypes = {
+PostListMedia.propTypes = {
   posts: PropTypes.array.isRequired,
   removeMediaUpdate: PropTypes.func.isRequired
 }
-export default PostList1
+export default PostListMedia
